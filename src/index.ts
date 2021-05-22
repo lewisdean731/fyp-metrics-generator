0c3d5fb2f420bab45b4a3949493173d307fbe990
Lewis Dean <lewisdean731@gmail.com>
2021-05-22 23:12:52 +0100

    feat: port helper functions from dependency-scanner
    [Ticket: FYP-41]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
 import { fetchEnvVar } from "./utils/envUtil";
-import { getAllProjectIds } from "./utils/apiUtil";
+import * as apiUtil from "./utils/apiUtil";
 
 const RUN_DELAY_SECONDS: number = parseInt(fetchEnvVar("RUN_DELAY_SECONDS"));
 
 const delay = (s: number) => {
   const ms = s * 1000;
   return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+// Compares current installed version to latest available
+export function isUsingLatestVersion(dependency: dependency): boolean {
+  let currentVersionArray = toVersionIntArray(dependency.version);
+  let latestVersionArray = toVersionIntArray(dependency.latest_version);
+
+  for (let i = 0; i < currentVersionArray.length; i++) {
+    if (currentVersionArray[i] !== latestVersionArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Return how long a version has been available for
+export function howOutOfDate(releaseDate: Date | number): number {
+  // https://stackoverflow.com/questions/2627650/why-javascript-gettime-is-not-a-function
+  let currentDate = new Date().getTime();
+  return currentDate - new Date(releaseDate).getTime();
+}
+
+// Splits version into an array e.g. '2.1.0' => ['2','1','0']
+// https://stackoverflow.com/questions/9914216/how-do-i-separate-an-integer-into-separate-digits-in-an-array-in-javascript
+const toVersionIntArray = (version: string): number[] => {
+  return version
+    .toString()
+    .split(".")
+    .map(function (t) {
+      return parseInt(t);
+    });
 };
 
 (async () => {
